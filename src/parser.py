e0f14b20ecb18ccaed6f5ab111b68cd26695d6b3
riley <60167783+rbarklow1998@users.noreply.github.com>
2021-10-27 04:46:02 -0400

    start of graphing

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,6 @@
+# %%
 import json,os
+import matplotlib.pyplot as plt
 from os import path
 
 # Main function
@@ -7,23 +9,36 @@ def main():
     # Attempts to form path where the data is located from current directory
     dataPath = os.path.join(os.path.dirname(__file__), os.pardir, 'data')
 
-    files = []
+    files = {}
 
     # Iterates through files in the data directory
     for file in os.listdir(dataPath):
 
         # Calls jsonToList on each file
-        files.append(jsonToList(os.path.join(dataPath, file)))
+        files[file] = (jsonToList(os.path.join(dataPath, file)))
 
-        # Prints the results
-        print(file,':')
-        for jsonList in files:
-            for list in jsonList:
-                for dict in list:
-                    for k, v in dict.items():
-                        print('   ',k, ' : ', v)
+    
+    for k,v in files.items():
+        totalTorrents = 0
+        name = ""
+        print(k,':     ')
+        for list in v:
+            for dict in list:
+                for k, v in dict.items():
+                    if "Torrents" in k:
+                        totalTorrents += int(v)
+                    if "Name" in k:
+                        name = v
 
-                    print('\n')
+                    print('   ',k.replace("\n",""), ' : ', v.replace("\n",""))
+
+        
+                print('\n')
+        print(name, ": ",totalTorrents,"\n")
+        plt.bar(1,totalTorrents,color='green')
+        plt.xlabel("Torrents")
+        plt.show()
+
 
 # Takes the path of each json file and returns it as a list of dictionaries
 def jsonToList(path):
@@ -40,3 +55,4 @@ def jsonToList(path):
 # Lets main be above function declarations
 if __name__ == "__main__":
     main()
+# %%
