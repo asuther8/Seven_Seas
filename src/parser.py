b525dbc14fb9a8dc370c6108c637442e84652436
Andrew-Banks-UT <77769993+Andrew-Banks-UT@users.noreply.github.com>
2021-11-13 16:18:06 -0500

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -2,24 +2,25 @@
 import json,os
 import matplotlib.pyplot as plt
 from os import path
-
-# Main function
 class show:
-    def __init__(self, name, service, total):
+    def __init__(self, name, service, total, date):
         self.name = name
-        self.service = service
         self.total = total
+        self.date = date
 class dataline:
-    def __init__(self, name, data, views, file){
+    def __init__(self, name, date, views, file){
         self.name = name
-        self.data = data
+        self.date = date
         self.views = views
         self.file = file
+
+# Main function
 def main():
 
     # Attempts to form path where the data is located from current directory
     dataPath = os.path.join(os.path.dirname(__file__), os.pardir, 'data')
-
+    list = []
+    
     files = {}
 
     # Iterates through files in the data directory
@@ -29,19 +30,32 @@ def main():
         files[file] = (jsonToList(os.path.join(dataPath, file)))
 
     torrentData = {}
+    showlist = []
+    inlist = 0
     for k,v in files.items():
         for list in v:
             oldestDate = 0
             for dict in list:
                 tempName = ""
                 tempScore = 0
-                
+
                 # iterating through each json file here
                 for k, v in dict.items():
-                    if "Torrents" in k:
-                        tempScore = int(v)
+                    line=dataline("temp", 0, 0, "temp")
+                    
                     if "Name" in k:
                         tempName = v.replace("\n","")
+                        for i in showlist:
+                            if i.name = tempName:
+                                inlist = 1
+                        if inlist == 0:
+                            showlist.append(show(tempName, 0, 0))
+                        line.name = tempName
+                    if "Torrents" in k:
+                        tempScore = int(v)
+                        for i in showlist:
+                            if i.name = tempName:
+                                i.views += tempScore
                     if "Date" in k:
                         splitDate = v.split()
                         if int(splitDate[2]) > oldestDate and splitDate[3] == "months":
@@ -51,9 +65,9 @@ def main():
                 else:
                     torrentData[tempName] = tempScore
 
-            
 
-            
+
+
 
 
 
