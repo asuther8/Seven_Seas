382d4143ae7d035b9fb822be654e1ac83b2061f9
riley <60167783+rbarklow1998@users.noreply.github.com>
2021-12-01 15:48:09 -0500

    fix stuff

--- src/parser.py
+++ src/parser.py
@@ -8,8 +8,9 @@ class show:
         self.service = service
         self.total = total
         self.date = date
+         
 class dataline:
-    def __init__(self, name, date, views, file){
+    def __init__(self, name, date, views, file):
         self.name = name
         self.date = date
         self.views = views
@@ -17,106 +18,77 @@ class dataline:
 
 # Main function
 def main():
+    s = 'Solid Torrents-'
+    e = '.json'
 
     # Attempts to form path where the data is located from current directory
     dataPath = os.path.join(os.path.dirname(__file__), os.pardir, 'data')
 
-    list = []
-    
-    files = []
-
-
+    showlist = {}
 
     # Iterates through files in the data directory
     for file in os.listdir(dataPath):
 
-
         # Calls jsonToList on each file
-        files[file] = (jsonToList(os.path.join(dataPath, file)))
-
-    torrentData = {}
-    showlist = []
-    inlist = 0
-    for k,v in files.items():
-        for list in v:
-            oldestDate = 0
-            for dict in list:
-                tempName = ""
-                tempScore = 0
-
+        if file != 'Solid_Torrents.json' and file != 'Solid_Torrents-Disney_16347609071.json' and file != 'ommision words.txt':
+            print(file)
+            newFile = jsonToList(os.path.join(dataPath, file))
+            print(newFile)
+            for dict in newFile:
                 # iterating through each json file here
+                tempName = ""
                 for k, v in dict.items():
-                    line=dataline("temp", 0, 0, "temp")
-                    
+
                     if "Name" in k:
                         tempName = v.replace("\n","")
-                        for i in showlist:
-                            if i.name = tempName:
-                                inlist = 1
-                        if inlist == 0:
-                            showlist.append(show(tempName, 0, 0))
-                        else: 
-                            inlist = 0
-                        line.name = tempName
+
+                        if tempName not in showlist:
+                            showlist[tempName] = show(tempName,"empty",0,0)
+
                     if "Torrents" in k:
                         tempScore = int(v)
-                        for i in showlist:
-                            if i.name = tempName:
-                                i.views += tempScore
+
+                        if tempName in showlist:
+                            showlist[tempName].total += tempScore
+                        else:
+                            showlist[tempName] = tempScore
+
                     if "Date" in k:
+
                         splitDate = v.split()
-                        if int(splitDate[2]) > oldestDate and splitDate[3] == "months":
-                            oldestDate = int(splitDate[2])
-                if tempName in torrentData:
-                    torrentData[tempName] += tempScore
-                else:
-                    torrentData[tempName] = tempScore
+                        time = 0
 
+                        if(splitDate[2] == "a"):
+                            time = 1
+                        else:
+                            time = int(splitDate[2])
 
+                        newAge = 0
+                        if splitDate[3] == "days" or splitDate[3] ==  "day":
+                            newAge = time * 1
+                        if splitDate[3] == "months" or splitDate[3] == "month":
+                            newAge = time * 30
+                        if splitDate[3] == "years" or splitDate[3] == "year":
+                            newAge = time * 365
 
+                        if newAge > showlist[tempName].date:
+                            showlist[tempName].date = newAge
+                
+                    showlist[tempName].service = file[file.find(s)+len(s):file.rfind(e)]
 
-        # used for testing
-        # if file == 'Solid Torrents-Prime.json':
+    for k,v in showlist.items():
+        print(k)
+        for var in vars(v):
+            print(var,": ",getattr(v,var))
+        print('')
 
-        # Calls jsonToList on each file
-            files.append((jsonToList(os.path.join(dataPath, file))))
-
-
-    #print(files)
-    torrentData = {}
-    for file in files:
-        #oldestDate = 0
-        for dict in file:
-            tempName = ""
-            tempScore = 0
-            # iterating through each json file here
-            for k, v in dict.items():
-                if "Torrents" in k:
-                    tempScore = int(v)
-                if "Name" in k:
-                    tempName = v.replace("\n","")
-                #if "Date" in k:
-                    #splitDate = v.split()
-                    #if int(splitDate[2]) > oldestDate and splitDate[3] == "months":
-                        #oldestDate = int(splitDate[2])
-
-            if tempName in torrentData:
-                torrentData[tempName] += tempScore
-            else:
-                torrentData[tempName] = tempScore
-
-            # calculating torrents per month
-        #if oldestDate != 0:
-            #torrentData[tempName] /= oldestDate
-
-    print(torrentData)
-    plt.style.use('ggplot')
-    x_pos = [i for i, _ in enumerate(torrentData.keys())]
-    plt.bar(x_pos,torrentData.values(),color='green')
-    plt.xlabel("Media torrented")
-    plt.ylabel("Torrents in millions")
-    plt.xticks(x_pos,torrentData.keys())
-    plt.show()
+    #plt.style.use('ggplot')
+    #x_pos = [i for i, _ in enumerate(showlist.keys())]
+    #plt.bar(x_pos,showlist.values(),color='green')
+    #plt.xlabel("Media torrented")
+    #plt.ylabel("Torrents in millions")
+    #plt.xticks(x_pos,showlist.keys())
+    #plt.show()
 
 # Takes the path of each json file and returns it as a list of dictionaries
 def jsonToList(path):
@@ -124,7 +96,6 @@ def jsonToList(path):
     with open(path, "r") as textFile:
         jsonString = textFile.read()
         return json.loads(jsonString)
-
 
 # Lets main be above function declarations
 if __name__ == "__main__":
